5dda83162638c08c495e58602f07d3dc8897271d
Irbel69 <marcel2005marcel@gmail.com>
2022-07-10 13:25:41 +0200

    Location Search Engine improved

--- src/scripts/search-engine.js
+++ src/scripts/search-engine.js
@@ -1,19 +1,25 @@
 const provider = new GeoSearch.OpenStreetMapProvider();
 
 const form = document.querySelector('form');
-const input = form.querySelector('#search-engine-input');
+const input = document.querySelector('#search-engine-input');
+const result = document.querySelector("#search-engine-results");
 
+form.addEventListener('submit', function (e){
+  e.preventDefault();
+});
 
-form.addEventListener('submit', async (event) => {
+input.addEventListener('input', async (e) => {
+  console.log(e);
+  result.innerHTML="<div class='loading-screen'><div class='loader'></div> <p>Searching results...</p></div>";
+  window.stop(); //in order to stop other previous requests
 
-  document.getElementById("search-engine-results").innerHTML = "";
-
-  event.preventDefault();
-
-  const results = await provider.search({ query: input.value });
+  const results = await provider.search({ query: input.value}); //this migth trhow an error if the order stop order has been executed;
+  if (results == []){
+    ;
+  }
+  result.innerHTML="";
   for (let i=0; i<results.length; i++){
-    console.log(results[i]);
-    document.getElementById("search-engine-results").innerHTML += `
+    result.innerHTML += `
     <div class='search-engine-location' id='location-${i}'>
       ${results[i].label}
     <div>
@@ -22,9 +28,14 @@ form.addEventListener('submit', async (event) => {
   for (let i=0; i<results.length; i++){
     document.getElementById(`location-${i}`).addEventListener("click", function(){
       const d = distance(results[i].bounds[0][0], results[i].bounds[1][0], results[i].bounds[0][1], results[i].bounds[1][1]);
-      map.flyTo([results[i].y, results[i].x], -((Math.log(d/40075))/(Math.log(2))));
-      document.getElementById("search-engine-results").innerHTML = "";
+      map.flyTo([results[i].y, results[i].x], (-((Math.log(d/40075))/(Math.log(2))))+1);
+      result.innerHTML = "";
     });
   }
 });
+
+input.addEventListener("keydown", function(e){
+  if (e.code == "Escape")
+    result.innerHTML = "";
+})
   
