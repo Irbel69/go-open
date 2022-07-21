26fee00bdc1a1c4dea5940d73bfcad502b0c438e
Irbel69 <marcel2005marcel@gmail.com>
2022-07-21 11:58:05 +0200

    No result for no search problem solved

--- src/scripts/searchEngine.js
+++ src/scripts/searchEngine.js
@@ -9,6 +9,7 @@ form.addEventListener('submit', function (e){
 });
 
 input.addEventListener('input', async (e) => {
+  
   result.innerHTML="<div class='loading-screen'><div class='loader'></div> <p>Searching results...</p></div>";
   var i1 = input.value;
   await sleep(200);
@@ -18,10 +19,13 @@ input.addEventListener('input', async (e) => {
   const results = await provider.search({ query: input.value}); //this migth trhow an error if the order stop order has been executed;
   console.log(results);
   result.innerHTML="";
-  if (results.length==0){
-    console.log("empty")
+
+  if (input.value == "")
+    result.innerHTML="";
+  else if (results.length==0)
     result.innerHTML="<div class='no-results-screen'><i class='fa-solid fa-magnifying-glass'></i><p>No results for this search :( <br>Contribute in <a href='https://www.openstreetmap.org' target='_blank'>OSM</a>!</p></div>";
-  }
+
+  
   for (let i=0; i<results.length; i++){
     result.innerHTML += `
     <div class='search-engine-location' id='location-${i}'>
@@ -29,6 +33,7 @@ input.addEventListener('input', async (e) => {
     <div>
     `.replace(input.value, `<b>${input.value}</b>`);
   }
+
   for (let i=0; i<results.length; i++){
     document.getElementById(`location-${i}`).addEventListener("click", function(){
       const d = distance(results[i].bounds[0][0], results[i].bounds[1][0], results[i].bounds[0][1], results[i].bounds[1][1]);
