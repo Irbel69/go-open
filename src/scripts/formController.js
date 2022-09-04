49e5c25dc7368551cc6f9341802e08bcb58a96b2
Marcel <marcel2005marcel@gmail.com>
2022-09-05 00:53:40 +0200

    Error in form displayed

--- src/scripts/formController.js
+++ src/scripts/formController.js
@@ -35,29 +35,61 @@ document.querySelectorAll(".select-location-button").forEach(element => {
 
 
 document.getElementById("calculate-route-button").addEventListener("click", async function(){
+    cleanError();
+    var valid = true;
+    var errorMessage = "";
+
+    travelOptions.algorithm = document.getElementById("select-1658324317245").value;
+    travelOptions.process = document.getElementById("show-process-checkbox").checked;    
+
     try {
         travelOptions.vehicle = document.querySelector(".route-btn-selected").id;
-        travelOptions.algorithm = document.getElementById("select-1658324317245").value;
-        travelOptions.process = document.getElementById("show-process-checkbox").checked;    
-        if (travelOptions.start.length == 0 || travelOptions.end.length == 0){
+    } catch (error) {
+        vehiclePanelError();
+        errorMessage += "❌ Please, select the vehicle.";
+        console.log(error);
+        valid = false;
+    }   
+    
+    try {
+        if (travelOptions.start.length == 0){
             const n = 1;
             n.replace(2,""); //You can't replace a int (force to catch)
         }
-        cleanError();
+    } catch (error) {
+        startPositionError();
+        errorMessage += "<br>❌ Please, select your initial position from the map.";
+        console.log(error);
+        valid = false;
+    }
+
+    try {
+        if (travelOptions.end.length == 0){
+            const n = 1;
+            n.replace(2,""); //You can't replace a int (force to catch)
+        }
+    } catch (error) {
+        destinationPositionError();
+        errorMessage += "<br>❌ Please, select your destination point from the map.";
+        console.log(error);
+        valid = false;
+    }
+
+
+    if (valid){
         await sleep(200);
         document.querySelector(".planning-route-panel").classList.toggle("active");
         document.querySelector(".other-buttons-panel").classList.toggle("disabled");
         buildRoute();
-    } catch (error) {
-        formError();
-        console.log(error);
     }
 });
 
 
-function formError(){
+function formError(errorMessage){
+    document.getElementById("form-error-message").innerHTML = errorMessage;
     document.getElementById("form-error-message").style.visibility = "inherit";
 }
+
 function cleanError(){
     document.getElementById("form-error-message").style.visibility = "hidden";
 }
@@ -93,4 +125,29 @@ async function getLatLonName(lat, lon){
         console.log(error);
     });
     return `${result.address.road}, ${result.address.house_number}`.replace(", undefined", "").replace("undefined", result.address.county).replace("undefined", result.address.state);
+}
+
+
+async function vehiclePanelError(){
+    document.querySelector(".route-buttons").style.backgroundColor = "#ff8c8c69";
+    document.querySelector(".route-buttons").style.color = "red";
+    await sleep(1000);
+    document.querySelector(".route-buttons").style.backgroundColor = "white";
+    document.querySelector(".route-buttons").style.color = "#707070";
+}
+
+async function startPositionError(){
+    document.querySelector("#start-location").style.backgroundColor = "#ff8c8c69";
+    document.querySelector(".location-input").style.color = "red";
+    await sleep(1000);
+    document.querySelector("#start-location").style.backgroundColor = "white";
+    document.querySelector(".location-input").style.color = "#707070";
+}
+
+async function destinationPositionError(){
+    document.querySelector("#end-location").style.backgroundColor = "#ff8c8c69";
+    document.querySelectorAll(".location-input")[1].style.color = "red";
+    await sleep(1000);
+    document.querySelector("#end-location").style.backgroundColor = "white";
+    document.querySelectorAll(".location-input")[1].style.color = "#707070";
 }
